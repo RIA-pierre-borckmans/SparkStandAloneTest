1d97ae5bb4c46a79a3be972c1fbfc2f34badd316
RIA-pierre-borckmans <RIA@Pierres-MacBook-Pro.local>
2014-01-30 14:31:01 +0100

    update

--- SparkTest.sbt
+++ SparkTest.sbt
@@ -10,5 +10,6 @@ libraryDependencies += "org.apache.spark" %% "spark-core" % "0.8.0-incubating"
 resolvers += "Akka Repository" at "http://repo.akka.io/releases"
 
 
+libraryDependencies += "org.apache.hadoop" % "hadoop-client" % "2.0.0-mr1-cdh4.4.0"
 
 
--- src/main/scala/SparkTest.scala
+++ src/main/scala/SparkTest.scala
@@ -3,7 +3,7 @@ import org.apache.spark.SparkContext._
 
 object SparkTest {
   def main(args: Array[String]) {
-    val sc = new SparkContext("spark://ria-soft-master:7180","SparkTest","/ria/spark/spark-0.8.0-incubating",List("target/scala-2.9.3/sparktest_2.9.3-1.0.jar"))
+    val sc = new SparkContext("spark://ria-soft-master:7077","SparkTest","/ria/spark/spark-0.8.0-incubating",List("target/scala-2.9.3/sparktest_2.9.3-1.0.jar"))
     val f = sc.textFile("hdfs://ria-soft-master/data/MTN_IC/CDR/2012_01/")
 
     println(f.count)
